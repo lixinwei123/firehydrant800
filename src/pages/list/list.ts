6e178e5a5caa9e04deefe796c9919fd546006cad
Cassandra Le <lenguyetminh1998@gmail.com>
2018-04-14 19:58:29 -0400

    DONE: add in map

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild} from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import {} from '@types/googlemaps';
 
 @Component({
   selector: 'page-list',
@@ -10,28 +11,24 @@ export class ListPage {
   icons: string[];
   items: Array<{title: string, note: string, icon: string}>;
 
+  @ViewChild('gmap') gmapElement: any;
+  maps: google.maps.Map;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-    // If we navigated to this page, we will have an item available as a nav param
-    this.selectedItem = navParams.get('item');
 
-    // Let's populate this page with some filler content for funzies
-    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-    'american-football', 'boat', 'bluetooth', 'build'];
 
-    this.items = [];
-    for (let i = 1; i < 11; i++) {
-      this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-      });
-    }
   }
 
-  itemTapped(event, item) {
-    // That's right, we're pushing to ourselves!
-    this.navCtrl.push(ListPage, {
-      item: item
-    });
+
+  //------- INIT MAP ------
+  ngOnInit() {
+    var mapProp = {
+      center: new google.maps.LatLng(18.5793, 73.8143),
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    this.maps = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
   }
+
+
 }
