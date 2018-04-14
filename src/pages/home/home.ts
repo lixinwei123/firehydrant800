b676ef49f55dfa9f6719d5ac66c75dd3a16b9977
lenguyentri1999 <michaelle7312@gmail.com>
2018-04-14 14:58:02 -0400

    Finished creating a fire

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ModalController } from 'ionic-angular';
+import { AutocompletePage } from './places-autocomplete';
+import { AngularFireDatabase } from 'angularfire2/database';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,34 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController
+    ,private modalCtrl: ModalController
+    ,private afData: AngularFireDatabase
 
+    ) {
+
+  }
+
+  openModal(){
+    let autocompleteModal = this.modalCtrl.create(AutocompletePage);
+    autocompleteModal.present();
+    autocompleteModal.onDidDismiss(data=> {
+      let addressData = data;
+      this.createFire(addressData);
+
+    })
+  }
+
+  createFire(addressData) {
+
+    let ref = this.afData.database.ref(`fires`)
+    let key = ref.push().key
+    let obj = {
+      address: addressData.address,
+      latlng: addressData.latlng,
+      id: key
+    }
+    ref.child(key).update(obj);
   }
 
 }
