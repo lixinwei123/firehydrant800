a1be371b69d620231b27e6c5e381a210f2d763bb
lixinwei123 <lixinwei123@yahoo.com>
2018-04-14 15:14:32 -0400

    ok

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { LaunchNavigator, LaunchNavigatorOptions } from '@ionic-native/launch-navigator';
+import { Geolocation } from '@ionic-native/geolocation';
 
 @Component({
   selector: 'page-home',
@@ -10,14 +11,25 @@ import { LaunchNavigator, LaunchNavigatorOptions } from '@ionic-native/launch-na
 
 //----------------- HOME PAGE CONSTRUCTOR -------------
 export class HomePage {
-
+  longitude: any;
+  latitude: any;
   constructor(public navCtrl: NavController,
-            private LaunchNavigator: LaunchNavigator) {
+            private LaunchNavigator: LaunchNavigator, private geolocation: Geolocation) {
                 console.log(LaunchNavigator);
+
 
   }
 
-
+getLocation(){
+  this.geolocation.getCurrentPosition().then((resp) => {
+ this.longitude = resp.coords.latitude;
+ this.latitude = resp.coords.latitude;
+ console.log(this.longitude,this.latitude);
+ // resp.coords.longitude
+}).catch((error) => {
+  console.log('Error getting location', error);
+});
+}
   //--------- LAUNCH DIRECTION -----------
   launchDirection(){
     console.log('This function is triggered');
