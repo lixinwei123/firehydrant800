cbd0b62fb81c19ce76bdca00e4062b28a963756e
lenguyentri1999 <michaelle7312@gmail.com>
2018-04-14 15:11:29 -0400

    Add fire

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,18 +2,26 @@ import { Component } from '@angular/core';
 import { NavController, ModalController } from 'ionic-angular';
 import { AutocompletePage } from './places-autocomplete';
 import { AngularFireDatabase } from 'angularfire2/database';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  fires$: any;
 
   constructor(public navCtrl: NavController
     ,private modalCtrl: ModalController
     ,private afData: AngularFireDatabase
 
     ) {
+      this.loadFires();
+
+  }
+
+  loadFires() {
+    this.fires$ = this.afData.list(`fires`).valueChanges()
 
   }
 
