ec01b4f79b4b37818799070f0b30cf4e301a89eb
Cassandra Le <lenguyetminh1998@gmail.com>
2018-04-14 15:02:04 -0400

    IN PROGRESS: calculate 2 distabces

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -22,8 +22,8 @@ export class HomePage {
   launchDirection(){
     console.log('This function is triggered');
       let options: LaunchNavigatorOptions = {
-      start: 'London, ON',
-      app: this.LaunchNavigator.APP.UBER
+      start: 'Philadelphia, PA',
+      // app: this.LaunchNavigator.APP
     };
 
     this.LaunchNavigator.navigate('Toronto, ON', options)
@@ -32,4 +32,28 @@ export class HomePage {
         error => console.log('Error launching navigator', error)
       );
   }
+
+
+  //----------- CALCULATE DISTANCE ----------
+  calculateDistance(lat1,lon1,lat2,lon2){
+
+      let R = 6371; // Radius of the earth in km
+      let dLat = this.deg2rad(lat2-lat1);  // deg2rad below
+      var dLon = this.deg2rad(lon2-lon1);
+      var a =Math.sin(dLat/2) * Math.sin(dLat/2) +
+        Math.cos(this.deg2rad(lat1)) * Math.cos(this.deg2rad(lat2)) *
+        Math.sin(dLon/2) * Math.sin(dLon/2);
+      var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+      var d = R * c; // Distance in km
+      console.log(d);
+      return d;
+  }
+
+
+  //----------- DEGREE CONVERSION ---------
+  deg2rad(deg) {
+      return deg * (Math.PI/180)
+  }
+
+
 }
