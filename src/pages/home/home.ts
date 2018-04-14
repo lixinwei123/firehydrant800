da7f8d8b129fc8e67458ed067e378b20128c255c
Cassandra Le <lenguyetminh1998@gmail.com>
2018-04-14 14:31:22 -0400

    DONE: launch navigator

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,35 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { LaunchNavigator, LaunchNavigatorOptions } from '@ionic-native/launch-navigator';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
+
+//----------------- HOME PAGE CONSTRUCTOR -------------
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,
+            private LaunchNavigator: LaunchNavigator) {
+                console.log(LaunchNavigator);
 
   }
 
+
+  //--------- LAUNCH DIRECTION -----------
+  launchDirection(){
+    console.log('This function is triggered');
+      let options: LaunchNavigatorOptions = {
+      start: 'London, ON',
+      app: this.LaunchNavigator.APP.UBER
+    };
+
+    this.LaunchNavigator.navigate('Toronto, ON', options)
+      .then(
+        success => console.log('Launched navigator'),
+        error => console.log('Error launching navigator', error)
+      );
+  }
 }
