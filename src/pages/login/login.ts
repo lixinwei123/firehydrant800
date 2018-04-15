3d2e75b225864de87129cb64a19f3ad9361f81cc
Cassandra Le <lenguyetminh1998@gmail.com>
2018-04-15 10:16:17 -0400

    Fixed login

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { HomePage } from '../home/home';
 
-@IonicPage()
+// @IonicPage()
 @Component({
   selector: 'page-login',
   templateUrl: 'login.html',
@@ -17,7 +17,7 @@ export class LoginPage {
   }
 
   move(){
-      this.navCtrl.push('HomePage');
+      this.navCtrl.setRoot(HomePage);
   }
 
 }
