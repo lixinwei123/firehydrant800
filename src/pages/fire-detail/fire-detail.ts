62bdb04212e0e486e15569ba557141d4ad42ed4d
Cassandra Le <lenguyetminh1998@gmail.com>
2018-04-14 21:57:25 -0400

    DONE: inuse done

--- src/pages/fire-detail/fire-detail.ts
+++ src/pages/fire-detail/fire-detail.ts
@@ -3,15 +3,9 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Geolocation } from '@ionic-native/geolocation';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { Http } from '@angular/http';
-import { Storage } from '@ionic/storage';
+import { LaunchNavigator, LaunchNavigatorOptions } from '@ionic-native/launch-navigator';
 
 
-/**
- * Generated class for the FireDetailPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 declare var google;
 
 @IonicPage()
@@ -25,16 +19,18 @@ export class FireDetailPage {
   latitude: any;
     fires$: any;
     hydData: any;
+    inuseInfo: any;
     bestHyd: {
       id: number,
       dist: number,
     };
      hydArray = [];
 
-  constructor(public navCtrl: NavController, private http: Http, public navParams: NavParams, private geolocation: Geolocation, private afData: AngularFireDatabase, private storage: Storage ) {
+  constructor(public navCtrl: NavController, private http: Http, public navParams: NavParams, private geolocation: Geolocation, private afData: AngularFireDatabase, public LaunchNavigator: LaunchNavigator) {
     this.fire = navParams.get('fire');
     console.log("Fire", this.fire)
     // this.getLocation();
+    this.getInuse()
     this.calcBest();
   }
 
@@ -43,8 +39,8 @@ export class FireDetailPage {
   }
 
 
-   //--------- CALCULATE THE BEST HYDRANT TO THE FIRE --------
   async calcBest(){
+
    await this.afData.database.ref('firehydrants').once('value',dataSnap =>{
       this.hydData = dataSnap.val();
     })
@@ -58,7 +54,8 @@ export class FireDetailPage {
         }
         if(this.hydData[i].Critical == false && this.hydData[i].OutOfService == false){
         this.hydArray.push(obj);
-        }
+
+      }
       }
         this.hydArray.sort(function(x,y){
           return x.dist - y.dist
@@ -66,13 +63,11 @@ export class FireDetailPage {
       this.hydArray = [this.hydArray[0],this.hydArray[1],this.hydArray[2],this.hydArray[3],this.hydArray[4]];
       console.log("all hyd",this.hydArray);
 
-
-      //--------- STORE CLOSEST ALL HYDRANTS IN NATIVE STORAGE --------
-      this.storage.set('hydArray', this.hydArray);
-
-      //------------------ REVERSE GEOENCODING THE FIRE HYDRANT ITEMS ------------
       for (let i in this.hydArray){
+        // this.hydArray[i].address = this.reverseGeocode(this.hydArray[i].lat, this.hydArray[i].lng)
+        // console.log(this.hydArray[i].address);
         let hyd = this.hydArray[i]
+
         let lat = hyd.lat
         let lng = hyd.lng
         let url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=AIzaSyAoVoi8F7YFDzp6tp5azw2oJDbSFaZPKn0`
@@ -80,16 +75,14 @@ export class FireDetailPage {
         let address = res.json().results[0].formatted_address;
         this.hydArray[i].address = address;
 
-        // this.http.get(url).subscribe(res=> {
-        //   let address = res.json().results[0].formatted_address
-        //   console.log(address)
-        //   this.hydArray[i].address = address;
-        // })
       }
   }
 
-  //------------------ FUNCTION TO CALCULATE THE DISTANCE BETWEEN TWO POINTS -------------
+
+
   calculateDistance(lat1,lon1,lat2,lon2){
+
+
       let R = 6371; // Radius of the earth in km
       let dLat = this.deg2rad(lat2-lat1);  // deg2rad below
       var dLon = this.deg2rad(lon2-lon1);
@@ -97,11 +90,36 @@ export class FireDetailPage {
         Math.cos(this.deg2rad(lat1)) * Math.cos(this.deg2rad(lat2)) *
         Math.sin(dLon/2) * Math.sin(dLon/2);
       var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-      var d = R * c;
+      var d = R * c; // Distance in km
+      //console.log(d);
       return d;
   }
  deg2rad(deg) {
       return deg * (Math.PI/180)
   }
+launchDirection(hyd){
+    console.log('This function is triggered');
+      let options: LaunchNavigatorOptions = {
+      start: 'Philadelphia, PA',
+      // app: this.LaunchNavigator.APP
+    };
+
+    this.LaunchNavigator.navigate('Toronto, ON', options)
+      .then(
+        success => console.log('Launched navigator'),
+        error => console.log('Error launching navigator', error)
+      );
+     var obj = {
+
+      }
+      obj[hyd.id] = true
+      this.afData.database.ref("inuse").update(obj);
+  }
+
+ getInuse(){
+   this.afData.database.ref('inuse').on("value",success =>{
+       this.inuseInfo = success.val()
+   })
+ }
 
 }
