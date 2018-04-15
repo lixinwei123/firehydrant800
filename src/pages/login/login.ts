94183f6c68623bd62c56b626b1364abc1153ad31
Cassandra Le <lenguyetminh1998@gmail.com>
2018-04-15 09:24:24 -0400

    DONE: final version of login page

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,12 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the LoginPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { HomePage } from '../home/home';
 
 @IonicPage()
 @Component({
@@ -20,6 +14,10 @@ export class LoginPage {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
+  }
+
+  move(){
+      this.navCtrl.push('HomePage');
   }
 
 }
